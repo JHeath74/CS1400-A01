0f07b8fbc02c5aaf801885ecac7ada392573dd42
Kyle <Jefforyheath@mail.weber.edu>
2022-06-20 11:48:32 -0600

    First Commit

---
+++ A01.java
@@ -0,0 +1,24 @@
+/*****************************************
+* Author : Jeffory Heath
+* Date : 08/31/2014
+* Assignment: A01 – Print
+******************************************/
+public class A01
+{
+      public static void main(String[] augs)
+      {
+         System.out.println("Title: What I hope to get out of this class.");
+         System.out.println(" ");
+         System.out.println("The best way that I've found to exceed and achieve my own goals is ");
+         System.out.println("to work for them.  Sometimes we only get one chance and I want  ");
+         System.out.println("work hard to achieve my dream and I see this class as a step to ");
+         System.out.println("getting me there.  What I learn and how I learn will not only help me");
+         System.out.println("in college.  It will continue to help me after school and in my life,");
+         System.out.println("career and family");   
+         System.out.println(" ");
+         System.out.println("Title: What I am willing to put into this class. ");
+         System.out.println(" ");
+         System.out.println("I have found two things that make a college class more enjoyable is hard work and a good relationship with my classmates.");
+         System.out.println("I'm hoping that by being pleasant with my classmates and helping them when it's appropriate that I can make things better for everyone.");
+         }
+}
